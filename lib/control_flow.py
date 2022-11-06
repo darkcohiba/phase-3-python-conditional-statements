82648e8c0e01b554a49dfeed9255f1864671781d
Sam <samuel.waters@gmail.com>
2022-11-05 19:10:55 -0600

    two left

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 
+# def admin_login(username, password):
+#     if username == 'admin' or username == 'ADMIN':
+#         if password == '12345':
+#             return 'Access granted'
+#         else:
+#             return 'Access denied'
+#     else:
+#         return 'Access denied'
+
 def admin_login(username, password):
-    # your code here
-    pass
+    if (username == 'admin' or username == 'ADMIN') and password == '12345':
+        return 'Access granted'
+    else:
+        return 'Access denied'
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature > 85:
+        return "It's too dang hot out there!"
+    elif temperature > 65:
+        return "It's perfect out there!"
+    elif temperature > 40:
+        return "It's a little chilly out there!"
+    else:
+        return "It's brisk out there!"
+    
 
 def fizzbuzz(num):
     # your code here
     pass
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    return (num1 + operation + num2)
+    
+
+
